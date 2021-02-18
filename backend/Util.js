16da34df803c204674560ca80149f47732423819
Tanja <tanja.hromin@gmail.com>
2021-02-18 14:36:03 +0100

    room name id simplify - numbers only

--- backend/Util.js
+++ backend/Util.js
@@ -1,6 +1,6 @@
 const makeId = length => {
     var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var characters = '0123456789';
     var charactersLength = characters.length;
     for (var i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
